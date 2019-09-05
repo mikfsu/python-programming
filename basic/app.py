f8d3ab35b81dffa2bbe051780d0cf6835081dd74
mikfsu <miknstu@gmail.com>
2019-09-06 00:33:46 +0200

    basic python

---
+++ basic/app.py
@@ -0,0 +1,29 @@
+course = "Python Programming"
+print(len(course))
+print(course[0:6])
+print(course[0])
+print(course[:8])
+menu = "Chicken \n meat \n rice"
+print(menu)
+first = "Md"
+last = "Khan"
+full = first+" "+last
+print(full)
+full_len = f"{len(first)} {len(last)}"
+print(full_len)
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.lstrip())
+print(course.find("Pro"))
+print("Pro" in course)
+print("kk" not in course)
+print(10+5)
+print(10/3)
+print(10//6)
+print(2**4)
+print(10 % 3)
+x = 12
+x = x+3
+print(x)
