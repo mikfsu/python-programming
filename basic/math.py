294ba3c668aef11c23e656b3163a203433587efe
mikfsu <miknstu@gmail.com>
2019-09-06 01:12:46 +0200

    Basic Python

---
+++ basic/math.py
@@ -0,0 +1,8 @@
+print(10+5)
+print(10/3)
+print(10//6)
+print(2**4)
+print(10 % 3)
+x = 12
+x = x+3
+print(x)
---
+++ math/math_op.py
@@ -0,0 +1,5 @@
+import math
+print(math.ceil(4.9))
+print(math.factorial(5))
+print(round(3.8))
+print(abs(-2.5))
