e662af8abfb3f01b242b512e06a021c95a3cfd8f
mikfsu <miknstu@gmail.com>
2019-09-10 01:42:23 +0200

    loop

---
+++ loop/for_else.py
@@ -0,0 +1,8 @@
+successful = False
+for x in range(3):
+    print("Attempt")
+    if successful:
+        print("successful")
+        break
+else:
+    print("Attempted and three times failed")
---
+++ loop/nested_loop.py
@@ -0,0 +1,3 @@
+for x in range(1, 11):
+    for y in range(2, 3):
+        print(f"{(x),(y)}")
